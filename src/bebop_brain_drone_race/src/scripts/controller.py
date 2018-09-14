6cbc506c02766a0ecb009ed0ebcfdf8a76745435
Elijah Sawyers <elijahsawyers@gmail.com>
2018-09-14 14:53:00 -0500

    First commit

---
+++ src/bebop_brain_drone_race/src/scripts/controller.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+#This is what will determine the bebop drone's direction and velocity from line error and EEG scans.
+import rospy
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
+import time
+
+class Controller():
+    def __init__(self):
+        rospy.init_node("controller", anonymous=True)
+        self.takeoff_pub = rospy.Publisher("/bebop/takeoff", Empty, queue_size=10)
+        self.land_pub = rospy.Publisher("/bebop/land", Empty, queue_size=10)
+        self.cmd_pub = rospy.Publisher("/bebop/cmd_vel", Twist, queue_size=10)
+
+    def takeoff(self):
+        print "takeoff"
+        message = Empty()
+        self.takeoff_pub.publish(message)
+
+    def land(self):
+        print "land"
+        message = Empty()
+        self.land_pub.publish(message)
+
+    def listen(self):
+        usr_input = ""
+
+        while usr_input != "q":
+            usr_input = raw_input("Takoff(t), Land(l), or Quit(q): ")
+            
+            if usr_input.lower() == "t":
+                bebop_controller.takeoff()
+            elif usr_input.lower() == "l":
+                bebop_controller.land()
+            elif usr_input.lower() == "q":
+                bebop_controller.land()
+                break
+            else:
+                break   
+
+
+if __name__ == "__main__":
+    controller = Controller()
+    controller.listen()
+
+        
---
+++ src/bebop_brain_drone_race/src/scripts/line_error.py
@@ -0,0 +1,7 @@
+# This is where all openCV-related things will happen.
+from sensor_msgs.msg import Image
+
+def line_error():
+	# OpenCV calculation
+	
+rospy.Subscriber("/bebop/image_raw", Image, line_error)
