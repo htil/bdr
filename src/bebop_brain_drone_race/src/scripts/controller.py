0852d52ad412ae2a33c4b15c8eaf1dd3c2126c97
Elijah Sawyers <ejsawyers@gmail.com>
2018-09-29 00:41:34 -0500

    Controller and sensor

--- src/bebop_brain_drone_race/src/scripts/controller.py
+++ src/bebop_brain_drone_race/src/scripts/controller.py
@@ -9,11 +9,11 @@ from geometry_msgs.msg import Twist
 class Controller():
     def __init__(self):
         rospy.init_node("controller", anonymous=True)
-	rospy.Subscriber("error", Float32, self.move_y)
+    rospy.Subscriber("error", Float32, self.move_y)
         self.takeoff_pub = rospy.Publisher("takeoff", Empty, queue_size=10)
         self.land_pub = rospy.Publisher("land", Empty, queue_size=10)
-	self.velocity_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-	self.velocity = Twist()
+        self.velocity_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        self.velocity = Twist()
 
     def takeoff(self):
         print "takeoff"
@@ -26,18 +26,18 @@ class Controller():
         self.land_pub.publish(message)
 
     def move_y(self, data):
-	if (data.data < 0):
-		self.velocity.linear.y = 0.25
-	else:
-		self.velocity.linear.y = -0.25
-	self.velocity_pub.publish(self.velocity)
-	
+    if (data.data < 0):
+        self.velocity.linear.y = 0.25
+    else:
+        self.velocity.linear.y = -0.25
+    self.velocity_pub.publish(self.velocity)
+    
     def move_x(self):
-	self.velocity.linear.x = 1.0
-	self.velocity_pub.publish(self.velocity)
-	time.sleep(1)
-	self.velocity.linear.x = 0.0
-	self.velcity_pub.publish(self.velocity)
+    self.velocity.linear.x = 1.0
+    self.velocity_pub.publish(self.velocity)
+    time.sleep(1)
+    self.velocity.linear.x = 0.0
+    self.velcity_pub.publish(self.velocity)
 
 if __name__ == "__main__":
     bebop_controller = Controller()
@@ -50,8 +50,8 @@ if __name__ == "__main__":
             bebop_controller.takeoff()
         elif usr_input.lower() == "l":
             bebop_controller.land()
-	elif usr_input.lower() == "f":
-	    bebop_controller.move_x()
+    elif usr_input.lower() == "f":
+        bebop_controller.move_x()
         elif usr_input.lower() == "q":
             bebop_controller.land()
             break
--- src/bebop_brain_drone_race/src/scripts/sensor.py
+++ src/bebop_brain_drone_race/src/scripts/sensor.py
@@ -5,21 +5,21 @@ from std_msgs.msg import Float32
 from LineError import LineError
 
 class Sensor():
-	def __init__(self):
-		rospy.init_node("sensor", anonymous=True)
-		rospy.Subscriber("image_raw", Image, self.calculate_error)
-		self.error_publisher = rospy.Publisher("error", Float32) 
-		self.error = 0
-	
-	def calculate_error(self, image):
-		self.error = LineError.GetError(image.data)[1]
+    def __init__(self):
+        rospy.init_node("sensor", anonymous=True)
+        rospy.Subscriber("image_raw", Image, self.calculate_error)
+        self.error_publisher = rospy.Publisher("error", Float32) 
+        self.error = 0
+    
+    def calculate_error(self, image):
+        self.error = LineError.GetError(image.data)[1]
 
-	def run(self):	
-		rate = rospy.Rate(10)
-		while not rospy.is_shutdown():
-			self.error_publisher.publish(self.error)
-			rate.sleep()
+    def run(self):	
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.error_publisher.publish(self.error)
+            rate.sleep()
 
 if __name__ == "__main__":	
-	sensor = Sensor()
-	sensor.run()
+    sensor = Sensor()
+    sensor.run()
