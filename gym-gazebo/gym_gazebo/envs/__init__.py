f2cc48a8da4c6eb76dd62dc5d75ce3eb7ec3b327
lcbentley <lcbentley@crimson.ua.edu>
2019-02-28 10:05:33 -0600

    add bdr catkin ws

--- gym-gazebo/gym_gazebo/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-from gym_gazebo.envs.gazebo_env import GazeboEnv
-from gym_gazebo.envs.real_env import RealEnv
-
-# completed: lcb 02/26/19 11:51:00 AM
--- gym-gazebo/gym_gazebo/envs/bebop/__init__.py
+++
@@ -1,3 +0,0 @@
-from gym_gazebo.envs.bebop.gazebo_bebop import GazeboBrainDroneRaceCameraEnv
-
-# completed: lcb 02/26/19 12:02 PM
