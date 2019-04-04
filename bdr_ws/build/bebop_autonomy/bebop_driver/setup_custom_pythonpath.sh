65c793fdc9a6f7305fecf220f2f8e37ba4a279f1
lcbentley <lcbentley@crimson.ua.edu>
2019-04-03 22:06:11 -0500

    remove build and devel dirs

--- bdr_ws/build/bebop_autonomy/bebop_driver/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/htil/bdr/bdr_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec "$@"
--- bdr_ws/devel/share/hector_uav_msgs/cmake/hector_uav_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(hector_uav_msgs_MSG_INCLUDE_DIRS "/home/htil/Documents/bdr-cfc8271e0834a2481b4018a760c212cd3ca15cc9/bdr_ws/src/hector_quadrotor/hector_uav_msgs/msg;/home/htil/Documents/bdr-cfc8271e0834a2481b4018a760c212cd3ca15cc9/bdr_ws/devel/share/hector_uav_msgs/msg")
-set(hector_uav_msgs_MSG_DEPENDENCIES actionlib_msgs;std_msgs;geometry_msgs)
